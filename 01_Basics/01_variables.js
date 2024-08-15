d8dc1bd2b8831c4d93f1876f02c425688247187a
SujataPahi2004 <sujataroli42.com>
2024-08-15 19:38:55 +0530

    some variable with keyword

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,13 @@
+ const accountID = 445674
+ let accountEmail = "sujatapahi9724@gmail.com" 
+ var accountPassword = "54645"
+ accountCity = "Jajpur"
+ 
+//  acountID = 2 // not alllowed
+
+accountEmail = "dulskalksdio@gmail.com"
+accountPassword = "54649"
+accountCity = "Jaipur"
+
+ console.log(accountID);
+ console.table([accountID, accountEmail, accountPassword, accountCity ])
