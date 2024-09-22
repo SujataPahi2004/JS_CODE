b8a320b4fca7d570a83958eaf2c994388f42feca
SujataPahi2004 <sujataroli42.com>
2024-09-22 12:33:37 +0530

    conversion of data types

--- 01_Basics/03_conversionDataType.js
+++ 01_Basics/03_conversionDataType.js
@@ -1,4 +1,23 @@
-let scor =33
+let scor ="33avf"
 
-console.log(typeof score);
-console.log(typeof(score));
+console.log(typeof scor);
+console.log(typeof(scor));
+
+let valueInNumber = Number(scor)
+console.log(typeof valueInNumber);
+console.log( valueInNumber);
+// "33" => 33
+// "33avf" => NaN
+//true => 1; false =>0
+
+let isLoggedIn= 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+// 1=> true; 0 => false
+//"" => false
+// "sujata" => true
+
+let someN =33
+let stringN = String(someN)
+console.log(stringN);
+console.log(typeof stringN);
